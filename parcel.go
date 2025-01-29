f2b891f3cd857f826ceddcab495ed456d30efcaf
NikPeg <peganov.nik@gmail.com>
2025-01-29 16:17:00 +0300

    set_del

--- parcel.go
+++ parcel.go
@@ -71,15 +71,16 @@ func (s ParcelStore) SetStatus(number int, status string) error {
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
-	// реализуйте обновление адреса в таблице parcel
-	// менять адрес можно только если значение статуса registered
-
-	return nil
+	_, err := s.db.Exec("UPDATE parcel SET address = :address WHERE number = :number AND status = :status",
+        sql.Named("address", address),
+        sql.Named("number", number),
+        sql.Named("status", ParcelStatusRegistered))
+    return err
 }
 
 func (s ParcelStore) Delete(number int) error {
-	// реализуйте удаление строки из таблицы parcel
-	// удалять строку можно только если значение статуса registered
-
-	return nil
+	_, err := s.db.Exec("DELETE parcel WHERE number = :number AND status = :status",
+        sql.Named("number", number),
+        sql.Named("status", ParcelStatusRegistered))
+    return err
 }
