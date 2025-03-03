6b184018914c0ff689b476c954146afa04cc6de6
Vlad Pincher <vlad.pin4er@yandex.ru>
2025-03-03 11:18:27 +0300

    first-iteration

--- parcel.go
+++ parcel.go
@@ -8,53 +8,152 @@ type ParcelStore struct {
 	db *sql.DB
 }
 
+func (s ParcelStore) LastInsertId() (any, error) {
+	panic("unimplemented")
+}
+
 func NewParcelStore(db *sql.DB) ParcelStore {
 	return ParcelStore{db: db}
 }
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
+	res, err := db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	if err != nil {
+		return 0, err
+	}
 
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
+	p := Parcel{}
+
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		return p, err
+	}
+	defer db.Close()
+
 	// реализуйте чтение строки по заданному number
 	// здесь из таблицы должна вернуться только одна строка
-
+	row := db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = :number", sql.Named("number", number))
 	// заполните объект Parcel данными из таблицы
-	p := Parcel{}
+	err = row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+	if err != nil {
+		return p, err
+	}
 
 	return p, nil
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
 	// реализуйте чтение строк из таблицы parcel по заданному client
 	// здесь из таблицы может вернуться несколько строк
 
+	rows, err := db.Query("SELECT number, client, status, address, created_at FROM parcel WHERE client = :client", sql.Named("client", client))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
 	// заполните срез Parcel данными из таблицы
 	var res []Parcel
+	p := Parcel{}
+
+	for rows.Next() {
+
+		err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, p)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
 
 	return res, nil
 }
 
 func (s ParcelStore) SetStatus(number int, status string) error {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
 	// реализуйте обновление статуса в таблице parcel
+	_, err = db.Exec("UPDATE parcel SET status = :status WHERE number = :number",
+		sql.Named("status", status),
+		sql.Named("number", number))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
 	// реализуйте обновление адреса в таблице parcel
 	// менять адрес можно только если значение статуса registered
+	_, err = db.Exec("UPDATE parcel SET address = :address WHERE number = :number AND status = :status",
+		sql.Named("address", address),
+		sql.Named("number", number),
+		sql.Named("status", ParcelStatusRegistered))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func (s ParcelStore) Delete(number int) error {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
 	// реализуйте удаление строки из таблицы parcel
 	// удалять строку можно только если значение статуса registered
+	_, err = db.Exec("DELETE FROM parcel WHERE number = :number AND status = :status",
+		sql.Named("number", number),
+		sql.Named("status", ParcelStatusRegistered))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
