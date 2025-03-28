7d6384b68cb8886c757f11d5bd4766e39fe25a20
Sergey <zaharov72729@gmail.com>
2025-03-28 15:36:48 +0300

    Created_at and "" in parcel

--- parcel.go
+++ parcel.go
@@ -14,12 +14,13 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-	res, err := s.db.Exec("INSERT INTO parcel(client, status, address, created_at)"+
-		"VALUES(:client, :status, :address, :created_At)",
+	res, err := s.db.Exec(
+		"INSERT INTO parcel(client, status, address, created_at)"+
+			"VALUES(:client, :status, :address, :created_at)",
 		sql.Named("client", p.Client),
-		sql.Named("status", "p.Status"),
-		sql.Named("address", "p.Address"),
-		sql.Named("createdAt", p.CreatedAt))
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
 	if err != nil {
 		return 0, err
 	}
@@ -35,8 +36,7 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
-	row := s.db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = :number",
-		sql.Named("number", number))
+	row := s.db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = :number", sql.Named("number", number))
 	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
 	if err != nil {
 		return Parcel{}, err
